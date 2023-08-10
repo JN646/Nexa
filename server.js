52ea0a0398ae802a8ee8c0d0f59489a1e4d48f9b
JN646 <josh.ginn@hotmail.co.uk>
2023-08-10 21:59:21 +0100

    Added CRUD functionality

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mysql = require('mysql');
 
 // System Settings
 const port = 3000;
+const consoleLogging = false;
 
 const app = express();
 
@@ -24,9 +25,56 @@ db.connect((err) => {
     console.log('MySQL Connected...');
 });
 
-// Nexa Core
+// Log all requests
+app.use((req, res, next) => {
+    // Console Logging
+    if (consoleLogging == true) {
+        console.log(req.method + ' ' + req.url);
+    }
+
+    next();
+});
+
+// Log all api requests to database
+app.use('/api', (req, res, next) => {
+    let post = {
+        log_method: req.method,
+        log_url: req.url
+    };
+
+    let sql = 'INSERT INTO api_logs SET ?';
+
+    let query = db.query(sql, post, (err, result) => {
+        if (err) {
+            throw err;
+        }
+    });
+
+    next();
+});
+
+// Home Page
 app.get('/', (req, res) => {
-  res.send('Nexa Core Initiated');
+    res.send('Nexa Core API');
+});
+
+// Status
+app.get('/status', (req, res) => {
+    // Get information about server
+    let server = {
+        name: 'ParaplannerNexa',
+        version: '1.0.0',
+        status: 'Running',
+        port: port
+    };
+
+    // Console Logging
+    if (consoleLogging == true) {
+        console.log(server);
+    }
+
+    // JSON Response
+    res.json(server);
 });
 
 // Nexa Core API
@@ -36,13 +84,17 @@ app.get('/api', (req, res) => {
 
 // Nexa Core API - Get All Cases
 app.get('/api/cases/all', (req, res) => {
-    let sql = 'SELECT * FROM work_case';
+    let sql = 'SELECT work_case.*, pp_firstname, pp_lastname, ad_firstname, ad_lastname FROM work_case LEFT JOIN paraplanners ON work_case.case_pp_id = paraplanners.pp_id LEFT JOIN advisers ON work_case.case_ad_id = advisers.ad_id';
 
     let query = db.query(sql, (err, results) => {
         if (err) {
             throw err;
         }
-        console.log(results);
+
+        // Console Logging
+        if (consoleLogging == true) {
+            console.log(results);
+        }
         
         // JSON Response
         res.json(results);
@@ -57,7 +109,74 @@ app.get('/api/cases/:id', (req, res) => {
         if (err) {
             throw err;
         }
-        console.log(result);
+
+        // Console Logging
+        if (consoleLogging == true) {
+            console.log(results);
+        }
+
+        // JSON Response
+        res.json(result);
+    });
+});
+
+// Create Case
+app.post('/api/cases/create', (req, res) => {
+    let post = {
+        case_pp_id: req.body.case_pp_id,
+        case_ad_id: req.body.case_ad_id,
+        case_due_date: req.body.case_due_date
+    };
+
+    let sql = 'INSERT INTO work_case SET ?';
+
+    let query = db.query(sql, post, (err, result) => {
+        if (err) {
+            throw err;
+        }
+
+        // Console Logging
+        if (consoleLogging == true) {
+            console.log(result);
+        }
+
+        // JSON Response
+        res.json(result);
+    });
+});
+
+// Update Case
+app.put('/api/cases/update/:id', (req, res) => {
+    let sql = `UPDATE work_case SET case_pp_id = '${req.body.case_pp_id}', case_ad_id = '${req.body.case_ad_id}', case_due_date = '${req.body.case_due_date}' WHERE case_id = ${req.params.id}`;
+
+    let query = db.query(sql, (err, result) => {
+        if (err) {
+            throw err;
+        }
+
+        // Console Logging
+        if (consoleLogging == true) {
+            console.log(result);
+        }
+
+        // JSON Response
+        res.json(result);
+    });
+});
+
+// Delete Case
+app.delete('/api/cases/delete/:id', (req, res) => {
+    let sql = `DELETE FROM work_case WHERE case_id = ${req.params.id}`;
+
+    let query = db.query(sql, (err, result) => {
+        if (err) {
+            throw err;
+        }
+
+        // Console Logging
+        if (consoleLogging == true) {
+            console.log(result);
+        }
 
         // JSON Response
         res.json(result);
@@ -72,7 +191,11 @@ app.get('/api/cases/paraplanner/:id', (req, res) => {
         if (err) {
             throw err;
         }
-        console.log(result);
+
+        // Console Logging
+        if (consoleLogging == true) {
+            console.log(results);
+        }
 
         // JSON Response
         res.json(result);
@@ -87,7 +210,11 @@ app.get('/api/cases/adviser/:id', (req, res) => {
         if (err) {
             throw err;
         }
-        console.log(result);
+
+        // Console Logging
+        if (consoleLogging == true) {
+            console.log(results);
+        }
 
         // JSON Response
         res.json(result);
@@ -102,7 +229,175 @@ app.get('/api/cases/paraplanner/none', (req, res) => {
         if (err) {
             throw err;
         }
-        console.log(result);
+
+        // Console Logging
+        if (consoleLogging == true) {
+            console.log(results);
+        }
+
+        // JSON Response
+        res.json(result);
+    });
+});
+
+// List of all Paraplanners
+app.get('/api/paraplanners/all', (req, res) => {
+    let sql = 'SELECT * FROM paraplanners';
+
+    let query = db.query(sql, (err, results) => {
+        if (err) {
+            throw err;
+        }
+
+        // Console Logging
+        if (consoleLogging == true) {
+            console.log(results);
+        }
+
+        // JSON Response
+        res.json(results);
+    });
+});
+
+// Create Paraplanner
+app.post('/api/paraplanners/create', (req, res) => {
+    let post = {
+        pp_firstname: req.body.pp_firstname,
+        pp_lastname: req.body.pp_lastname,
+        pp_email: req.body.pp_email
+    };
+
+    let sql = 'INSERT INTO paraplanners SET ?';
+
+    let query = db.query(sql, post, (err, result) => {
+        if (err) {
+            throw err;
+        }
+
+        // Console Logging
+        if (consoleLogging == true) {
+            console.log(result);
+        }
+
+        // JSON Response
+        res.json(result);
+    });
+});
+
+// Update Paraplanner
+app.put('/api/paraplanners/update/:id', (req, res) => {
+    let sql = `UPDATE paraplanners SET pp_firstname = '${req.body.pp_firstname}', pp_lastname = '${req.body.pp_lastname}', pp_email = '${req.body.pp_email}' WHERE pp_id = ${req.params.id}`;
+
+    let query = db.query(sql, (err, result) => {
+        if (err) {
+            throw err;
+        }
+
+        // Console Logging
+        if (consoleLogging == true) {
+            console.log(result);
+        }
+
+        // JSON Response
+        res.json(result);
+    });
+});
+
+// Delete Paraplanner
+app.delete('/api/paraplanners/delete/:id', (req, res) => {
+    let sql = `DELETE FROM paraplanners WHERE pp_id = ${req.params.id}`;
+
+    let query = db.query(sql, (err, result) => {
+        if (err) {
+            throw err;
+        }
+
+        // Console Logging
+        if (consoleLogging == true) {
+            console.log(result);
+        }
+
+        // JSON Response
+        res.json(result);
+    });
+});
+
+// List of all Advisers
+app.get('/api/advisers/all', (req, res) => {
+    let sql = 'SELECT * FROM advisers';
+
+    let query = db.query(sql, (err, results) => {
+        if (err) {
+            throw err;
+        }
+
+        // Console Logging
+        if (consoleLogging == true) {
+            console.log(results);
+        }
+
+        // JSON Response
+        res.json(results);
+    });
+});
+
+// Create Adviser
+app.post('/api/advisers/create', (req, res) => {
+    let post = {
+        ad_firstname: req.body.ad_firstname,
+        ad_lastname: req.body.ad_lastname,
+        ad_email: req.body.ad_email
+    };
+
+    let sql = 'INSERT INTO advisers SET ?';
+
+    let query = db.query(sql, post, (err, result) => {
+        if (err) {
+            throw err;
+        }
+
+        // Console Logging
+        if (consoleLogging == true) {
+            console.log(result);
+        }
+
+        // JSON Response
+        res.json(result);
+    });
+});
+
+// Update Adviser
+app.put('/api/advisers/update/:id', (req, res) => {
+    let sql = `UPDATE advisers SET ad_firstname = '${req.body.ad_firstname}', ad_lastname = '${req.body.ad_lastname}', ad_email = '${req.body.ad_email}' WHERE ad_id = ${req.params.id}`;
+
+    let query = db.query(sql, (err, result) => {
+        if (err) {
+            throw err;
+        }
+
+        // Console Logging
+        if (consoleLogging == true) {
+            console.log(result);
+        }
+
+        // JSON Response
+        res.json(result);
+    });
+});
+
+// Delete Adviser
+app.delete('/api/advisers/delete/:id', (req, res) => {
+    let sql = `DELETE FROM advisers WHERE ad_id = ${req.params.id}`;
+
+    let query = db.query(sql, (err, result) => {
+        if (err) {
+            throw err;
+        }
+
+        // Console Logging
+        if (consoleLogging == true) {
+            console.log(result);
+        }
 
         // JSON Response
         res.json(result);
