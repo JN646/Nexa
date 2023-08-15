3bffa7507208f5d3f506f5e227ab70345d3a057d
JN646 <josh.ginn@hotmail.co.uk>
2023-08-15 18:56:10 +0100

    Add loading message

--- client2/src/CaseList.js
+++ client2/src/CaseList.js
@@ -5,56 +5,60 @@ import BidCount from './BidCount';
 
 const CaseList = () => {
     const [cases, setCases] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        // Fetch cases from the API
-        axios.get('http://localhost:3005/api/cases/unassigned', {
-            headers: {
-                'x-api-key': '6bc32663-fb4f-4b8b-86e7-f08faa2cf302'
-            }
-        })
-            .then(response => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:3005/api/cases/unassigned', {
+                    headers: {
+                        'x-api-key': '6bc32663-fb4f-4b8b-86e7-f08faa2cf302'
+                    }
+                });
                 setCases(response.data);
-            })
-            .catch(error => {
+                setLoading(false);
+            } catch (error) {
                 console.error('Error fetching cases:', error);
-            });
+            }
+        };
+        fetchData();
     }, []);
+
     return (
         <div>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Type</th>
-                        <th>Adviser Name</th>
-                        <th>Case Created</th>
-                        <th>Due Date</th>
-                        <th>Bids</th>
-                        <th>Info</th>
-                        {/* Add other case properties you want to display */}
-                    </tr>
-                </thead>
-                <tbody>
-                    {cases.map(caseItem => (
-                        <tr key={caseItem.case_id}>
-                            <td>{caseItem.case_id}</td>
-                            <td>{caseItem.case_type}</td>
-                            <td>{caseItem.ad_firstname} {caseItem.ad_lastname}</td>
-                            <td>{new Date(caseItem.case_created_at).toLocaleDateString('en-GB')}</td>
-                            {caseItem.case_due_date ? (
-                                <td>{new Date(caseItem.case_due_date).toLocaleDateString('en-GB')}</td>
-                            ) : (
-                                <td></td>
-                            )}
-                            <td><BidCount bidCaseId={caseItem.case_id} /></td>
-                            <td>
-                                <CaseModal caseId={caseItem.case_id} />
-                            </td>
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Type</th>
+                            <th>Adviser Name</th>
+                            <th>Case Created</th>
+                            <th>Due Date</th>
+                            <th>Bids</th>
+                            <th>Info</th>
+                            {/* Add other case properties you want to display */}
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {cases.map(({ case_id, case_type, ad_firstname, ad_lastname, case_created_at, case_due_date }) => (
+                            <tr key={case_id}>
+                                <td>{case_id}</td>
+                                <td>{case_type}</td>
+                                <td>{`${ad_firstname} ${ad_lastname}`}</td>
+                                <td>{new Date(case_created_at).toLocaleDateString('en-GB')}</td>
+                                <td>{case_due_date ? new Date(case_due_date).toLocaleDateString('en-GB') : ''}</td>
+                                <td><BidCount bidCaseId={case_id} /></td>
+                                <td>
+                                    <CaseModal caseId={case_id} />
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
         </div>
     );
 };
