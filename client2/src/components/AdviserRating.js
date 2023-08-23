e451c94afad06c58f036c9f508e74aa48800da62
JN646 <josh.ginn@hotmail.co.uk>
2023-08-23 12:35:51 +0100

    Fixed adviser rating

--- client2/src/components/AdviserRating.js
+++ client2/src/components/AdviserRating.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Rating from '@mui/material/Rating';
 
-const AdviserRating = ({ adviserID }) => {
+const AdviserRating = ({ adviserId }) => {
     const [adviserRating, setAdviserRating] = useState(0);
 
     useEffect(() => {
         // Fetch bid count from the API for the specific bid_case_id
-        axios.get(`http://localhost:3005/api/advisers/star-rating/${adviserID}`, {
+        axios.get(`http://localhost:3005/api/advisers/star-rating/${adviserId}`, {
             headers: {
                 'x-api-key': '6bc32663-fb4f-4b8b-86e7-f08faa2cf302'
             }
@@ -15,22 +16,13 @@ const AdviserRating = ({ adviserID }) => {
                 setAdviserRating(response.data[0].rating);
             })
             .catch(error => {
-                console.error('Error fetching bid count:', error);
+                console.error('Error fetching rating:', error);
             });
-    }, [adviserID]);
-
-    const stars = [];
-    for (let i = 0; i < 5; i++) {
-        if (i < adviserRating) {
-            stars.push(<i key={i} className="fa-solid fa-star"></i>);
-        } else {
-            stars.push(<i key={i} className="fa-regular fa-star"></i>);
-        }
-    }
-
+    }, [adviserId]);
+    
     return (
         <span>
-            {stars}
+            <Rating name="read-only" value={adviserRating} readOnly />
         </span>
     );
 };
