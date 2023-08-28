9c597400c9c863875914e922957e2d73a3791a29
JN646 <josh.ginn@hotmail.co.uk>
2023-08-28 17:32:09 +0100

    Increase api throttle limit

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.disable('x-powered-by')
 // Define a rate limiter to prevent excessive API requests
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
+  max: 1000, // Limit each IP to 100 requests per windowMs
 });
 app.use("/api", apiLimiter);
 
